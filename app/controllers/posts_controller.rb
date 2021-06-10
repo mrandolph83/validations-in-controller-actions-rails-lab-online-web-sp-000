9d5fa737713a1372d940924228f8b7e1f51ecf8a
mrandolph83 <mrandolph83@gmail.com>
2021-06-10 00:10:16 -0400

    commit tree 65730692935773b6b4bfc425fd3cbef22b5163ae

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,19 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
+    # @post = Post.find(params[:id])
   end
 
   def edit
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to(@post)
+    else
+    render :edit 
+    end
   end
 
   private
